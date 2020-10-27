a17255f897688e26d3a34250f0556cfec955bf4f
Genevieve Coogan <gcoogan1@gmail.com>
2020-10-27 17:15:23 -0400

    guesword tests

---
+++ src/intergration.test.js
@@ -0,0 +1,25 @@
+import { storeFactory } from '../Test/testUtils';
+import {guessWord} from './store/actions/index';
+
+describe("guessWord action dispatcher",() => {
+    
+    describe('no guessed words', () => {
+        
+        test('updates state correctly for an unsuccessful guess', () => {
+            
+        })
+        test('updates state correctly for a successful guess', () => {
+
+        })
+    })
+
+    describe('some guessed words', () => {
+        
+        test('updates state correctly for an unsuccessful guess', () => {
+            
+        })
+        test('updates state correctly for a successful guess', () => {
+
+        })
+    })
+})

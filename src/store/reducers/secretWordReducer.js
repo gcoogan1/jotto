089f19660f2dcdb70b488de9b26e2b5c6a227409
Genevieve Coogan <gcoogan1@gmail.com>
2020-10-28 13:07:52 -0400

    moxios

--- src/store/reducers/secretWordReducer.js
+++ src/store/reducers/secretWordReducer.js
@@ -1,5 +1,11 @@
+import { actionTypes } from "../actions/index";
 
 //returning state so that if state is already set then the state will not be overrided to null and the reducer will just return the state as is
 export default (state=null, action) => {
-    return state;
-}
+    switch (action.type) {
+      case actionTypes.SET_SECRET_WORD:
+        return action.payload;
+      default:
+        return state;
+    }
+  }
